f9b3cbebd30156addf87df0f11085fbe5460c32b
Andrej Benedik <abenedik@ab4d.com>
2024-09-13 11:25:43 +0200

    Added link to CadImporter sample

---
+++ Ab3d.DXEngine.Wpf.Samples/Other/CadImporterSample.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ab3d.DXEngine.Wpf.Samples.Other
+{
+    /// <summary>
+    /// Interaction logic for CadImporterSample.xaml
+    /// </summary>
+    public partial class CadImporterSample : Page
+    {
+        public CadImporterSample()
+        {
+            InitializeComponent();
+
+            SampleImage.Cursor = Cursors.Hand;
+            SampleImage.MouseLeftButtonUp += SampleImage_OnMouseLeftButtonUp;
+        }
+
+        private void Hyperlink_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenCadImporterGitHub();
+        }
+
+        private void SampleImage_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            OpenCadImporterGitHub();
+        }
+
+        private void OpenCadImporterGitHub()
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo("https://github.com/ab4d/CadImporter") { UseShellExecute = true });
+        }
+    }
+}
